86fb2dfe53914dd6aa0f5aff1dc76e5c96573ab8
SaraAlajmy <saraalagami@gmail.com>
2024-07-12 22:15:31 +0300

    http

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -59,8 +59,11 @@ export class DetailsComponent {
 
   constructor() {
     const housingLocationId = Number(this.route.snapshot.paramMap.get('id'));
-    this.housingLocation =
-      this.housingService.getHousingLocationById(housingLocationId);
+    this.housingService
+      .getHousingLocationById(housingLocationId)
+      .then((housingLocation) => {
+        this.housingLocation = housingLocation;
+      });
   }
 
   submitApplication() {
